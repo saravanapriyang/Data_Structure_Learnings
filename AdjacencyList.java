b887f7d312c6d75a50e38f88d0727c0de481335d
Padma Priyan <saravanapriyan30122003@gmail.com>
2024-03-11 14:34:24 +0530

    added adjacency list in java program

---
+++ AdjacencyList.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+
+class Graph{
+    LinkedList<Integer> [] adj;
+
+    @SuppressWarnings("unchecked")
+    public Graph(int node){
+        this.adj = new LinkedList[node];
+        for(int i =0;i<node;i++){
+            this.adj[i]= new LinkedList<>();
+
+        }     
+    }
+
+    public void addEdge(int u, int v){   
+        adj[u].add(v);       
+        adj[v].add(u);
+    }
+    void display(){
+        for(int i =0;i<4;i++){
+            System.out.print("Vertex " + i + ": ");
+            for(Integer edge : adj[i]){
+                System.out.print(" " + edge);
+            }
+            System.out.println();
+        }
+    }
+}
+
+public class AdjacencyList {
+    public static void main(String[] args) {
+        Graph g = new Graph(4);
+        g.addEdge(0, 1);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(3, 0);
+
+        g.display();
+    }
+
+}
