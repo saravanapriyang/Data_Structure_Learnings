21a23ebb0039e3a708127a65664c1d16c57ac879
SARAVANA PRIYAN G <162957646+saravanapriyang@users.noreply.github.com>
2024-03-12 11:24:37 +0530

    Create Dfs.java

---
+++ Dfs.java
@@ -0,0 +1,77 @@
+import java.util.Stack;
+import java.util.LinkedList;
+
+class Graph {
+    LinkedList<Integer>[] adj;
+
+    @SuppressWarnings("unchecked")
+    public Graph(int node) {
+        this.adj = new LinkedList[node];
+        for (int i = 0; i < node; i++) {
+            this.adj[i] = new LinkedList<>();
+
+        }
+    }
+public void Dfs(int s,int vertex) {
+    boolean[] visited = new boolean[vertex];
+    Stack<Integer> Stack = new Stack<>();
+    Stack.push(s);
+    while(!Stack.isEmpty()){
+        int u = Stack.pop();
+        if(!visited[u]){
+            visited[u]= true;
+            System.out.println(u+" ");
+            for(int v: adj[u]){
+            if(!visited[v]){
+                Stack.push(v);
+            }
+            }
+        }
+    }
+
+}
+
+public void addEdge(int u, int v) {
+    adj[u].add(v);
+    adj[v].add(u);
+}
+
+void display() {
+    for (int i = 0; i < 4; i++) {
+        System.out.print("Vertex " + i + ": ");
+        for (Integer edge : adj[i]) {
+            System.out.print(" " + edge);
+        }
+        System.out.println();
+    }
+}
+}
+
+public class Dfs {
+public static void main(String[] args) {
+    Graph g = new Graph(4);
+    g.addEdge(0, 1);
+    g.addEdge(1, 2);
+    g.addEdge(2, 3);
+    g.addEdge(3, 0);
+
+    g.display();
+    System.out.println("");
+    g.Dfs(0, 4);
+}
+
+}
+//output
+
+//[Running] cd "c:\Users\padma\OneDrive\Desktop\Git\Java_Program_Learnings\" && javac Dfs.java && java Dfs
+//Vertex 0:  1 3
+//Vertex 1:  0 2
+//Vertex 2:  1 3
+//Vertex 3:  2 0
+
+//0 
+//3 
+//2 
+//1 
+
+//[Done] exited with code=0 in 2.055 seconds
